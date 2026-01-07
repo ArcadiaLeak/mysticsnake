0412e84db7bb059c3c93b4ac32d9ee73dc66305a
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-01-08 00:29:33 +0300

    move code 2

--- app.d
+++ app.d
@@ -1,10 +1,12 @@
 import lsg.vk.vulkan;
 import lsg.vk.Device;
 import lsg.app.Viewer;
+import lsg.threading.Latch;
 
 void main() {
   auto deviceIDPool = DeviceIDPool();
   auto viewer = new Viewer();
+  auto latch = new Latch(0);
 
   vkDeviceWaitIdle(cast(VkDevice) 0);
 }
