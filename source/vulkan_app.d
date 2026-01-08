5993cf39144ca3b8a65fa8282b5db4455082535b
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-01-09 00:56:12 +0300

    swapchain + imageviews

--- source/vulkan_app.d
+++ source/vulkan_app.d
@@ -1,9 +1,11 @@
 import core.stdc.string;
 import std.string;
+import std.algorithm.comparison;
 import std.algorithm.iteration;
 import std.stdio;
 import std.array;
 import std.typecons;
+import std.range;
 import std.range.primitives;
 import std.container.rbtree;
 
@@ -86,6 +88,8 @@ private:
     createSurface();
     pickPhysicalDevice();
     createLogicalDevice();
+    createSwapChain();
+    createImageViews();
   }
 
   const(char*)[] getRequiredExtensions() {
@@ -256,8 +260,8 @@ private:
   }
 
   struct QueueFamilyIndices {
-    Nullable!size_t graphicsFamily;
-    Nullable!size_t presentFamily;
+    Nullable!uint graphicsFamily;
+    Nullable!uint presentFamily;
       
     bool isComplete() {
       return !graphicsFamily.isNull && !presentFamily.isNull;
@@ -296,13 +300,15 @@ private:
     auto queueFamilies = new VkQueueFamilyProperties[queueFamilyCount];
     vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.ptr);
 
-    foreach (i, const ref queueFamily; queueFamilies) {
+    foreach (idx, const ref queueFamily; queueFamilies) {
+      uint i = cast(uint) idx;
+
       if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
         indices.graphicsFamily = i;
       }
       
       VkBool32 presentSupport = false;
-      vkGetPhysicalDeviceSurfaceSupportKHR(device, cast(uint) i, m_surface, &presentSupport);
+      vkGetPhysicalDeviceSurfaceSupportKHR(device, i, m_surface, &presentSupport);
       if (presentSupport) {
         indices.presentFamily = i;
       }
@@ -322,7 +328,7 @@ private:
     auto availableExtensions = new VkExtensionProperties[extensionCount];
     vkEnumerateDeviceExtensionProperties(device, null, &extensionCount, availableExtensions.ptr);
     
-    auto requiredExtensions = redBlackTree(deviceExtensions.map!(fromStringz));
+    auto requiredExtensions = redBlackTree(deviceExtensions.map!fromStringz);
     
     foreach (const ref ext; availableExtensions) {
       requiredExtensions.removeKey(ext.extensionName.fromStringz);
@@ -357,7 +363,7 @@ private:
     QueueFamilyIndices indices = findQueueFamilies(m_physicalDevice);
     
     VkDeviceQueueCreateInfo[] queueCreateInfos;
-    auto uniqueQueueFamilies = redBlackTree!size_t(
+    auto uniqueQueueFamilies = redBlackTree!uint(
       indices.graphicsFamily.get,
       indices.presentFamily.get
     );
@@ -366,7 +372,7 @@ private:
     foreach (queueFamily; uniqueQueueFamilies) {
       VkDeviceQueueCreateInfo queueCreateInfo;
       queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-      queueCreateInfo.queueFamilyIndex = cast(uint) queueFamily;
+      queueCreateInfo.queueFamilyIndex = queueFamily;
       queueCreateInfo.queueCount = 1;
       queueCreateInfo.pQueuePriorities = &queuePriority;
       queueCreateInfos ~= queueCreateInfo;
@@ -393,8 +399,142 @@ private:
       throw new Exception("Failed to create logical device!");
     }
     
-    vkGetDeviceQueue(m_device, cast(uint) indices.graphicsFamily.get, 0u, &m_graphicsQueue);
-    vkGetDeviceQueue(m_device, cast(uint) indices.presentFamily.get, 0u, &m_presentQueue);
+    vkGetDeviceQueue(m_device, indices.graphicsFamily.get, 0u, &m_graphicsQueue);
+    vkGetDeviceQueue(m_device, indices.presentFamily.get, 0u, &m_presentQueue);
+  }
+
+  void createSwapChain() {
+    SwapChainSupportDetails swapChainSupport = querySwapChainSupport(m_physicalDevice);
+    
+    VkSurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
+    VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
+    VkExtent2D extent = chooseSwapExtent(swapChainSupport.capabilities);
+
+    uint imageCount = swapChainSupport.capabilities.minImageCount + 1;
+    if (
+      swapChainSupport.capabilities.maxImageCount > 0 && 
+      imageCount > swapChainSupport.capabilities.maxImageCount
+    ) {
+      imageCount = swapChainSupport.capabilities.maxImageCount;
+    }
+
+    VkSwapchainCreateInfoKHR createInfo;
+    createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    createInfo.surface = m_surface;
+    createInfo.minImageCount = imageCount;
+    createInfo.imageFormat = surfaceFormat.format;
+    createInfo.imageColorSpace = surfaceFormat.colorSpace;
+    createInfo.imageExtent = extent;
+    createInfo.imageArrayLayers = 1;
+    createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+    QueueFamilyIndices indices = findQueueFamilies(m_physicalDevice);
+    uint[2] queueFamilyIndices = [
+      indices.graphicsFamily.get(),
+      indices.presentFamily.get()
+    ];
+
+    if (indices.graphicsFamily != indices.presentFamily) {
+      createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+      createInfo.queueFamilyIndexCount = 2;
+      createInfo.pQueueFamilyIndices = queueFamilyIndices.ptr;
+    } else {
+      createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+      createInfo.queueFamilyIndexCount = 0;
+      createInfo.pQueueFamilyIndices = null;
+    }
+
+    createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+    createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    createInfo.presentMode = presentMode;
+    createInfo.clipped = VK_TRUE;
+    createInfo.oldSwapchain = cast(VkSwapchainKHR) VK_NULL_HANDLE;
+
+    if (vkCreateSwapchainKHR(m_device, &createInfo, null, &m_swapchain) != VK_SUCCESS) {
+      throw new Exception("Failed to create swap chain!");
+    }
+    
+    vkGetSwapchainImagesKHR(m_device, m_swapchain, &imageCount, null);
+    m_swapchainImages.length = imageCount;
+    vkGetSwapchainImagesKHR(m_device, m_swapchain, &imageCount, m_swapchainImages.ptr);
+    
+    m_swapchainImageFormat = surfaceFormat.format;
+    m_swapchainExtent = extent;
+  }
+
+  VkSurfaceFormatKHR chooseSwapSurfaceFormat(in VkSurfaceFormatKHR[] availableFormats) {
+    foreach (const ref availableFormat; availableFormats) {
+      if (
+        availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && 
+        availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR
+      ) {
+        return availableFormat;
+      }
+    }
+    return availableFormats.front;
+  }
+
+  VkPresentModeKHR chooseSwapPresentMode(in VkPresentModeKHR[] availablePresentModes) {
+    foreach (const ref availablePresentMode; availablePresentModes) {
+      if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+        return availablePresentMode;
+      }
+    }
+    return VK_PRESENT_MODE_FIFO_KHR;
+  }
+
+  VkExtent2D chooseSwapExtent(in VkSurfaceCapabilitiesKHR capabilities) {
+    if (capabilities.currentExtent.width != uint.max) {
+      return capabilities.currentExtent;
+    } else {
+      int width, height;
+      glfwGetFramebufferSize(m_window, &width, &height);
+      
+      VkExtent2D actualExtent = {
+        width,
+        height
+      };
+      
+      actualExtent.width = clamp(
+        actualExtent.width, 
+        capabilities.minImageExtent.width, 
+        capabilities.maxImageExtent.width
+      );
+      actualExtent.height = clamp(
+        actualExtent.height, 
+        capabilities.minImageExtent.height, 
+        capabilities.maxImageExtent.height
+      );
+      
+      return actualExtent;
+    }
+  }
+
+  void createImageViews() {
+    m_swapchainImageViews.length = m_swapchainImages.length;
+    
+    foreach (ref swapchainImageTup; zip(m_swapchainImages, m_swapchainImageViews)) {
+      VkImageViewCreateInfo createInfo;
+      createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+      createInfo.image = swapchainImageTup[0];
+      createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+      createInfo.format = m_swapchainImageFormat;
+      
+      createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+      
+      createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+      createInfo.subresourceRange.baseMipLevel = 0;
+      createInfo.subresourceRange.levelCount = 1;
+      createInfo.subresourceRange.baseArrayLayer = 0;
+      createInfo.subresourceRange.layerCount = 1;
+      
+      if (vkCreateImageView(m_device, &createInfo, null, &swapchainImageTup[1]) != VK_SUCCESS) {
+        throw new Exception("Failed to create image views!");
+      }
+    }
   }
 
   void mainLoop() {
