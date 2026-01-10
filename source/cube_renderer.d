b4d5c6dba4629e95f7acfa281225a1d8863e402e
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-01-10 18:01:59 +0300

    correct winding order

--- source/cube_renderer.d
+++ source/cube_renderer.d
@@ -50,7 +50,7 @@ public:
 
   static immutable ushort[] s_indices = [
     0, 1, 2,  2, 3, 0,    // Front face
-    4, 5, 6,  6, 7, 4,    // Back face
+    4, 7, 6,  6, 5, 4,    // Back face
     8, 9, 10, 10, 11, 8,  // Left face
     12, 13, 14, 14, 15, 12, // Right face
     16, 17, 18, 18, 19, 16, // Top face
