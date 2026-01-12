45c8ba6e8ef8a7f9b1254cea69bbfc9131c46b06
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-01-12 16:05:37 +0300

    synchronized fence

--- source/scenegraph/vk/fence.d
+++ source/scenegraph/vk/fence.d
@@ -2,8 +2,8 @@ module scenegraph.vk.fence;
 
 import scenegraph.vk.vulkan;
 
-class Fence {
+synchronized class Fence {
   private VkFence _vkFence;
 
-  shared(VkFence) vk() shared { return _vkFence; }
+  shared(VkFence) vk() { return _vkFence; }
 }
