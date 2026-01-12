5b7f99fd9a9e312d5acffa0c9e883ed879aa94fb
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-01-12 14:55:16 +0300

    correct shared cast

--- source/scenegraph/vk/queue.d
+++ source/scenegraph/vk/queue.d
@@ -4,25 +4,25 @@ import scenegraph.vk.fence;
 import scenegraph.vk.vulkan;
 
 synchronized class Queue {
-private:
-  VkQueue _vkQueue;
-  VkQueueFlags _queueFlags;
-  uint32_t _queueFamilyIndex;
-  uint32_t _queueIndex;
+  private {
+    VkQueue _vkQueue;
+    VkQueueFlags _queueFlags;
+    uint32_t _queueFamilyIndex;
+    uint32_t _queueIndex;
 
-  this(
-    shared VkQueue queue,
-    VkQueueFlags queueFlags,
-    uint queueFamilyIndex,
-    uint queueIndex
-  ) {
-    _vkQueue = queue;
-    _queueFlags = queueFlags;
-    _queueFamilyIndex = queueFamilyIndex;
-    _queueIndex = queueIndex;
+    this(
+      VkQueue queue,
+      VkQueueFlags queueFlags,
+      uint queueFamilyIndex,
+      uint queueIndex
+    ) {
+      _vkQueue = cast(shared) queue;
+      _queueFlags = queueFlags;
+      _queueFamilyIndex = queueFamilyIndex;
+      _queueIndex = queueIndex;
+    }
   }
 
-public:
   VkResult waitIdle() {
     return vkQueueWaitIdle(cast(VkQueue) _vkQueue);
   }
