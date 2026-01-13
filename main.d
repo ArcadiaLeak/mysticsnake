d3ef32b9d1def58cd238faf43e8453b440da5e6d
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-01-14 01:04:37 +0300

    SDL_ShaderCross_CompileGraphicsShaderFromSPIRV

--- main.d
+++ main.d
@@ -5,72 +5,6 @@ import vulkan;
 import std.string;
 import std.stdio;
 
-void createVertexShader() {
-  string vertexShaderSource =
-    "#version 310 es\n" ~
-    "precision highp float;\n" ~
-    "\n" ~
-    "layout(location = 0) in vec3 inPosition;\n" ~
-    "\n" ~
-    "layout(std140, binding = 0) uniform UniformBufferObject {\n" ~
-    "  mat4 model;\n" ~
-    "  mat4 view;\n" ~
-    "  mat4 proj;\n" ~
-    "} ubo;\n" ~
-    "\n" ~
-    "void main() {\n" ~
-    "  gl_Position = ubo.proj * ubo.view * ubo.model * vec4(inPosition, 1.0);\n" ~
-    "}\n";
-
-  auto vertShaderCode = compileGLSLtoSPIRV(
-    vertexShaderSource,
-    "cube.vert",
-    true
-  );
-
-  auto vertexMetadata = SDL_ShaderCross_ReflectGraphicsSPIRV(
-    cast(const(ubyte)*) vertShaderCode,
-    vertShaderCode.length * uint.sizeof,
-    0
-  );
-  scope(exit) SDL_free(vertexMetadata);
-
-  writeln(vertexMetadata.resource_info.num_samplers);
-  writeln(vertexMetadata.resource_info.num_storage_textures);
-  writeln(vertexMetadata.resource_info.num_storage_buffers);
-  writeln(vertexMetadata.resource_info.num_uniform_buffers);
-}
-
-void createFragmentShader() {
-  string fragmentShaderSource =
-    "#version 310 es\n" ~
-    "precision highp float;\n" ~
-    "\n" ~
-    "layout(location = 0) out vec4 outColor;\n" ~
-    "\n" ~
-    "void main() {\n" ~
-    "  outColor = vec4(1.0, 0.5, 0.2, 1.0);\n" ~
-    "}\n";
-
-  auto fragShaderCode = compileGLSLtoSPIRV(
-    fragmentShaderSource,
-    "cube.frag",
-    false
-  );
-
-  auto fragMetadata = SDL_ShaderCross_ReflectGraphicsSPIRV(
-    cast(const(ubyte)*) fragShaderCode,
-    fragShaderCode.length * uint.sizeof,
-    0
-  );
-  scope(exit) SDL_free(fragMetadata);
-
-  writeln(fragMetadata.resource_info.num_samplers);
-  writeln(fragMetadata.resource_info.num_storage_textures);
-  writeln(fragMetadata.resource_info.num_storage_buffers);
-  writeln(fragMetadata.resource_info.num_uniform_buffers);
-}
-
 void main() {
   SDL_Init(SDL_INIT_VIDEO);
   scope(exit) SDL_Quit();
@@ -94,8 +28,88 @@ void main() {
   scope(exit) SDL_ReleaseWindowFromGPUDevice(device, window);
 
   glslangInitialize();
-  scope(exit) glslangFinalize();
 
-  createVertexShader();
-  createFragmentShader();
+  string vertexShaderSource =
+    "#version 310 es\n" ~
+    "precision highp float;\n" ~
+    "\n" ~
+    "layout(location = 0) in vec3 inPosition;\n" ~
+    "\n" ~
+    "layout(std140, binding = 0) uniform UniformBufferObject {\n" ~
+    "  mat4 model;\n" ~
+    "  mat4 view;\n" ~
+    "  mat4 proj;\n" ~
+    "} ubo;\n" ~
+    "\n" ~
+    "void main() {\n" ~
+    "  gl_Position = ubo.proj * ubo.view * ubo.model * vec4(inPosition, 1.0);\n" ~
+    "}\n";
+
+  auto vertShaderCode = compileGLSLtoSPIRV(
+    vertexShaderSource,
+    "cube.vert",
+    true
+  );
+  auto vertMetadata = SDL_ShaderCross_ReflectGraphicsSPIRV(
+    cast(const(ubyte)*) vertShaderCode,
+    vertShaderCode.length * uint.sizeof,
+    0
+  );
+  auto vertSpirvInfo = SDL_ShaderCross_SPIRV_Info(
+    cast(const(ubyte)*) vertShaderCode,
+    vertShaderCode.length * uint.sizeof,
+    "main",
+    SDL_ShaderCross_ShaderStage.SDL_SHADERCROSS_SHADERSTAGE_VERTEX,
+    0
+  );
+  auto vertexShader = SDL_ShaderCross_CompileGraphicsShaderFromSPIRV(
+    device,
+    &vertSpirvInfo,
+    &vertMetadata.resource_info,
+    0
+  );
+  SDL_free(vertMetadata);
+
+  writeln(vertexShader);
+  SDL_ReleaseGPUShader(device, vertexShader);
+
+  string fragmentShaderSource =
+    "#version 310 es\n" ~
+    "precision highp float;\n" ~
+    "\n" ~
+    "layout(location = 0) out vec4 outColor;\n" ~
+    "\n" ~
+    "void main() {\n" ~
+    "  outColor = vec4(1.0, 0.5, 0.2, 1.0);\n" ~
+    "}\n";
+
+  auto fragShaderCode = compileGLSLtoSPIRV(
+    fragmentShaderSource,
+    "cube.frag",
+    false
+  );
+  auto fragMetadata = SDL_ShaderCross_ReflectGraphicsSPIRV(
+    cast(const(ubyte)*) fragShaderCode,
+    fragShaderCode.length * uint.sizeof,
+    0
+  );
+  auto fragSpirvInfo = SDL_ShaderCross_SPIRV_Info(
+    cast(const(ubyte)*) fragShaderCode,
+    fragShaderCode.length * uint.sizeof,
+    "main",
+    SDL_ShaderCross_ShaderStage.SDL_SHADERCROSS_SHADERSTAGE_FRAGMENT,
+    0
+  );
+  auto fragShader = SDL_ShaderCross_CompileGraphicsShaderFromSPIRV(
+    device,
+    &fragSpirvInfo,
+    &fragMetadata.resource_info,
+    0
+  );
+  SDL_free(fragMetadata);
+
+  writeln(fragShader);
+  SDL_ReleaseGPUShader(device, fragShader);
+
+  glslangFinalize();
 }
