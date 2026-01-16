8b93e577871346ec0f979133dd46fdb64495034f
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-01-16 10:12:42 +0300

    event subscribers

--- yoga/event.d
+++ yoga/event.d
@@ -1,4 +1,7 @@
+import core.atomic;
 import std.traits;
+
+import yoga.node;
 
 enum LayoutPassReason : int {
   kInitial = 0,
@@ -22,6 +25,35 @@ struct LayoutData {
     measureCallbackReasonsCount;
 };
 
+private {
+  immutable struct SubscriberNode {
+    Event.Subscriber subscriber;
+    SubscriberNode* next;
+  }
+
+  shared(SubscriberNode*) subscribers = null;
+
+  SubscriberNode* push(SubscriberNode* newHead) @safe {
+    SubscriberNode* oldHead = null;
+
+    do {
+      oldHead = subscribers.atomicLoad!(MemoryOrder.raw);
+      if (newHead !is null) {
+        newHead = new SubscriberNode(
+          newHead.subscriber,
+          oldHead
+        );
+      }
+    } while (
+      casWeak!
+        (MemoryOrder.rel, MemoryOrder.raw)
+        (&subscribers, oldHead, newHead) != true
+    );
+
+    return oldHead;
+  }
+}
+
 struct Event {
   enum Type {
     NodeAllocation,
@@ -35,11 +67,13 @@ struct Event {
     NodeBaselineEnd,
   }
 
-  struct TypedData(Type T : Type.NodeAllocation) {
-    const Node config;
+  alias Subscriber = void delegate(const Node, Type);
+
+  void reset() {
+    push(null);
   }
 
-  struct TypedData(Type T : Type.NodeDeallocation) {
-    const Node config;
+  void subscribe(Subscriber subscriber) {
+    push(new SubscriberNode(subscriber));
   }
 }
