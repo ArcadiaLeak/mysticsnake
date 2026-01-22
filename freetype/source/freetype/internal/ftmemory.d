1583056003fb1f72432993fe58ef873848eb244e
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-01-22 12:54:15 +0300

    ftmemory format

--- freetype/source/freetype/internal/ftmemory.d
+++ freetype/source/freetype/internal/ftmemory.d
@@ -4,20 +4,16 @@ import freetype;
 
 import std.conv;
 
-string FT_FREE(string ptr) =>
-  FT_MEM_FREE(ptr);
+string FT_FREE(string ptr) => FT_MEM_FREE(ptr);
+string FT_DEBUG_INNER(string exp) => iq{ ($(exp)) }.text;
 
 string FT_MEM_FREE(string ptr) {
-  string debugInnerRet = FT_DEBUG_INNER(
-    iq{ft_mem_free( memory, ($(ptr)) )}.text
-  );
-
   return iq{
     $(FT_BEGIN_STMNT)
-      $(debugInnerRet);
+      $(FT_DEBUG_INNER(
+        iq{ft_mem_free( memory, ($(ptr)) )}.text
+      ));
       ($(ptr)) = null;
     $(FT_END_STMNT)
   }.text;
 }
-
-string FT_DEBUG_INNER(string exp) => iq{ ($(exp)) }.text;
