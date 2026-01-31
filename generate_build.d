7c6908f99899357d9f508777f8b70c3aca4dad61
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-01-31 04:06:12 +0300

    glslang_tab to draft

--- generate_build.d
+++ generate_build.d
@@ -8,52 +8,29 @@ import std.stdio : writeln;
 import std.path;
 import std.string;
 
+enum string prjname = "mysticsnake";
+
 void main() {
   string Makefile;
 
   immutable string[] libtargets = ["glslang"];
 
-  Makefile ~= "all: build/libmysticsnake_glslang.a\n";
+  Makefile ~= "all:";
+
+  foreach (target; libtargets) {
+    Makefile ~= i" build/lib$(target)_$(prjname).a".text;
+  }
+
+  Makefile ~= "\n";
 
   foreach (libname; libtargets) {
-    string[] srcPaths = libname
+    string srcPaths = libname
       .dirEntries("*.d", SpanMode.breadth, false)
       .map!(e => e.name)
-      .array;
-    string[] hdrPaths;
-    string[] objPaths;
+      .join(" ");
 
-    foreach (srcPath; srcPaths) {
-      string hdrPath = "build/interface/" ~ srcPath.setExtension(".di");
-      hdrPaths ~= hdrPath;
-
-      Makefile ~= "\n" ~ i"$(hdrPath): $(srcPath)\n".text ~
-        "\t" ~ "dmd $^ -Hf=$@ -o- -dip1000\n";
-    }
-
-    foreach (srcPath; srcPaths) {
-      string objPath = "build/" ~ srcPath.setExtension(".o");
-      objPaths ~= objPath;
-
-      string dependencyPath(string hdrPath) {
-        if(
-          hdrPath.chompPrefix("build/interface/").stripExtension ==
-          srcPath.stripExtension
-        ) {
-          return srcPath;
-        }
-
-        return hdrPath;
-      }
-
-      string dependencyStr = hdrPaths.map!(dependencyPath).join(" ");
-
-      Makefile ~= "\n" ~ i"$(objPath): $(dependencyStr)\n".text ~
-        "\t" ~ "dmd $^ -of=$@ -c -dip1000\n";
-    }
-
-    Makefile ~= "\n" ~ i"build/libmysticsnake_$(libname).a: $(objPaths.join(" "))\n".text ~
-      "\t" ~ "dmd $^ -of=$@ -debug -lib -dip1000\n";
+    Makefile ~= "\n" ~ i"build/lib$(libname)_$(prjname).a: $(srcPaths)\n".text ~
+      "\t" ~ "dmd -dip1000 -debug -lib $^ -of=$@\n";
   }
 
   write("Makefile", Makefile);
