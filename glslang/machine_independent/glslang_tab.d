9361f9d2095a72e61bbbc32cbdf9b87a1e22d584
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-02-07 18:07:40 +0300

    ctfe

--- glslang/machine_independent/glslang_tab.d
+++ glslang/machine_independent/glslang_tab.d
@@ -1,40 +1,23 @@
 module glslang.machine_independent.glslang_tab;
 
-symbol_t[] symbols;
-symbol_t[] symbols_sorted;
-symbol_t[string] symbol_table;
-
-item_number_t[] ritem;
-int nritems = 0;
-
-rule_t[] rules;
-rule_number_t nrules = 0;
-rule_t[][][] derives;
-
-int nnterms = 0;
-int ntokens = 1;
-int nsyms = 0;
-
-item_index_t[][] kernel_base;
-item_index_t[] kernel_items;
-
-bool[][] fderives;
-item_index_t[] itemset;
-bool[] ruleset;
-
-static this() {
-  gram_init().exec;
-  derives_compute();
-  generate_states();
-}
+static parser_meta = new const parser_init();
 
 void main() {
   import std.stdio;
 
-  writeln(ntokens);
-  writeln(nnterms);
-  writeln(nrules);
-  writeln(nritems);
+  writeln(parser_meta.ntokens);
+  writeln(parser_meta.nnterms);
+  writeln(parser_meta.nrules);
+  writeln(parser_meta.nritems);
+}
+
+alias item_number_t = int;
+
+alias rule_number_t = int;
+struct rule_t {
+  rule_number_t number;
+  sym_content_t lhs;
+  item_number_t[] rhs;
 }
 
 alias symbol_number_t = int;
@@ -88,205 +71,229 @@ class symbol_t {
   }
 }
 
-alias rule_number_t = int;
-alias item_number_t = int;
-alias item_index_t = uint;
+class parser_init {
+  symbol_t[] symbols;
+  symbol_t[] symbols_sorted;
+  symbol_t[string] symbol_table;
 
-struct rule_t {
-  rule_number_t number;
-  sym_content_t lhs;
-  item_number_t[] rhs;
-}
+  item_number_t[] ritem;
+  int nritems = 0;
 
-void derives_compute() {
-  import std.range;
+  rule_t[] rules;
+  rule_number_t nrules = 0;
+  rule_t[][][] derives;
 
-  struct rule_list_t {
-    rule_list_t[] next;
-    rule_t[] value;
+  int nnterms = 0;
+  int ntokens = 1;
+  int nsyms = 0;
+
+  item_index_t[][] kernel_base;
+  item_index_t[] kernel_items;
+
+  bool[][] fderives;
+  item_index_t[] itemset;
+  bool[] ruleset;
+
+  this() {
+    new gram_init(this);
+    derives_compute();
+    generate_states();
   }
 
-  rule_list_t[][] dset = new rule_list_t[][nnterms];
-  rule_list_t[] delts = new rule_list_t[nrules];
+  alias item_index_t = uint;
 
-  for (rule_number_t r = nrules - 1; r >= 0; --r) {
-    symbol_number_t lhs = rules[r].lhs.number;
-    rule_list_t[] p = delts[r..$];
+  void derives_compute() {
+    import std.range;
 
-    p.front.next = dset[lhs - ntokens];
-    p.front.value = rules[r..$];
+    struct rule_list_t {
+      rule_list_t[] next;
+      rule_t[] value;
+    }
 
-    dset[lhs - ntokens] = p;
-  }
+    rule_list_t[][] dset = new rule_list_t[][nnterms];
+    rule_list_t[] delts = new rule_list_t[nrules];
 
-  derives = new rule_t[][][nnterms];
-  rule_t[][] q = new rule_t[][nnterms + nrules];
+    for (rule_number_t r = nrules - 1; r >= 0; --r) {
+      symbol_number_t lhs = rules[r].lhs.number;
+      rule_list_t[] p = delts[r..$];
 
-  for (symbol_number_t i = ntokens; i < nsyms; ++i) {
-    rule_list_t[] p = dset[i - ntokens];
-    derives[i - ntokens] = q;
+      p.front.next = dset[lhs - ntokens];
+      p.front.value = rules[r..$];
 
-    while (p) {
-      q.front = p.front.value;
+      dset[lhs - ntokens] = p;
+    }
+
+    derives = new rule_t[][][nnterms];
+    rule_t[][] q = new rule_t[][nnterms + nrules];
+
+    for (symbol_number_t i = ntokens; i < nsyms; ++i) {
+      rule_list_t[] p = dset[i - ntokens];
+      derives[i - ntokens] = q;
+
+      while (p) {
+        q.front = p.front.value;
+        q.popFront;
+        p = p.front.next;
+      }
+
+      q.front = null;
       q.popFront;
-      p = p.front.next;
-    }
-
-    q.front = null;
-    q.popFront;
-  }
-}
-
-void print_derives() {
-  import std.range.primitives;
-  import std.stdio;
-
-  write("DERIVES\n");
-
-  for (symbol_number_t i = ntokens; i < nsyms; ++i) {
-    writef("  %s derives\n", symbols[i].tag);
-    for (rule_t[][] rp = derives[i - ntokens]; rp.front; rp.popFront) {
-      writef("    %3d ", rp.front.front.number);
-      if (rp.front.front.rhs.front >= 0)
-        for (item_number_t[] rhsp = rp.front.front.rhs; rhsp.front >= 0; rhsp.popFront)
-          writef(" %s", symbols[rhsp.front].tag);
-      else
-        writef(" %s", cast(dchar) 0x03b5);
-      write("\n");
     }
   }
 
-  write("\n\n");
-}
+  void print_derives() {
+    import std.range.primitives;
+    import std.stdio;
 
-void generate_states() {
-  allocate_storage();
-  closure_new(nritems);
-}
+    write("DERIVES\n");
 
-void closure_new(int n) {
-  itemset = new item_index_t[n];
-  ruleset = new bool[nrules];
-
-  set_fderives();
-}
-
-void set_fderives() {
-  import std.range.primitives;
-
-  bool[] buffer = new bool[nnterms * nrules];
-  fderives = new bool[][nnterms];
-  {
-    size_t j;
-    for (size_t i = 0; i < nnterms; i++) {
-      j = i + 1;
-      fderives[i] = buffer[i * nrules..j * nrules];
-    }
-  }
-
-  bool[][] firsts;
-  set_firsts(firsts);
-
-  for (symbol_number_t i = ntokens; i < nsyms; ++i)
-    for (symbol_number_t j = ntokens; j < nsyms; ++j)
-      if (firsts[i - ntokens][j - ntokens])
-        for (rule_number_t k = 0; derives[j - ntokens][k]; ++k)
-          fderives[i - ntokens][derives[j - ntokens][k].front.number] = true;
-}
-
-void print_fderives() {
-  import std.range.primitives;
-  import std.stdio;
-
-  write("FDERIVES\n");
-
-  for (symbol_number_t i = ntokens; i < nsyms; ++i) {
-    writef("  %s derives\n", symbols[i].tag);
-    foreach (r, flag; fderives[i - ntokens])
-      if (flag) {
-        writef("    %3d ", r);
-        if (rules[r].rhs.front >= 0)
-          for (item_number_t[] rhsp = rules[r].rhs; rhsp.front >= 0; rhsp.popFront)
+    for (symbol_number_t i = ntokens; i < nsyms; ++i) {
+      writef("  %s derives\n", symbols[i].tag);
+      for (rule_t[][] rp = derives[i - ntokens]; rp.front; rp.popFront) {
+        writef("    %3d ", rp.front.front.number);
+        if (rp.front.front.rhs.front >= 0)
+          for (item_number_t[] rhsp = rp.front.front.rhs; rhsp.front >= 0; rhsp.popFront)
             writef(" %s", symbols[rhsp.front].tag);
         else
           writef(" %s", cast(dchar) 0x03b5);
         write("\n");
       }
-  }
-
-  write("\n\n");
-}
-
-void set_firsts(out bool[][] firsts) {
-  import std.range.primitives;
-
-  bool[] buffer = new bool[nnterms * nnterms];
-  firsts = new bool[][nnterms];
-  {
-    size_t j;
-    for (size_t i = 0; i < nnterms; i++) {
-      j = i + 1;
-      firsts[i] = buffer[i * nnterms..j * nnterms];
-    }
-  }
-  
-  for (symbol_number_t i = ntokens; i < nsyms; ++i)
-    for (symbol_number_t j = 0; derives[i - ntokens][j]; ++j) {
-      item_number_t sym = derives[i - ntokens][j].front.rhs.front;
-      if(sym >= ntokens)
-        firsts[i - ntokens][sym - ntokens] = true;
-    }
-  
-  for (size_t i = 0; i < firsts.length; i++)
-    for (size_t j = 0; j < firsts.length; j++)
-      if (firsts[j][i])
-        for (size_t k = 0; k < firsts[j].length; k++)
-          firsts[j][k] = firsts[j][k] || firsts[i][k];
-
-  for (size_t i = 0; i < firsts.length; i++)
-    firsts[i][i] = true;
-}
-
-void print_firsts(in bool[][] firsts) {
-  import std.stdio;
-
-  write("FIRSTS\n");
-
-  for (symbol_number_t i = ntokens; i < nsyms; ++i) {
-    writef("  %s firsts\n", symbols[i].tag);
-    foreach (j, flag; firsts[i - ntokens])
-      if (flag) writef("    %s\n", symbols[j + ntokens].tag);
-  }
-
-  write("\n\n");
-}
-
-void allocate_storage() {
-  allocate_itemsets();
-}
-
-void allocate_itemsets() {
-  size_t count = 0;
-  size_t[] symbol_count = new size_t[nsyms];
-
-  for (rule_number_t r = 0; r < nrules; ++r)
-    for (size_t i = 0; rules[r].rhs[i] >= 0; i++) {
-      symbol_number_t sym = rules[r].rhs[i];
-      count += 1;
-      symbol_count[sym] += 1;
     }
 
-  kernel_base = new item_index_t[][nsyms];
-  kernel_items = new item_index_t[count];
+    write("\n\n");
+  }
 
-  count = 0;
-  for (symbol_number_t i = 0; i < nsyms; i++) {
-    kernel_base[i] = kernel_items[count..$];
-    count += symbol_count[i];
+  void generate_states() {
+    allocate_storage();
+    closure_new(nritems);
+  }
+
+  void closure_new(int n) {
+    itemset = new item_index_t[n];
+    ruleset = new bool[nrules];
+
+    set_fderives();
+  }
+
+  void set_fderives() {
+    import std.range.primitives;
+
+    bool[] buffer = new bool[nnterms * nrules];
+    fderives = new bool[][nnterms];
+    {
+      size_t j;
+      for (size_t i = 0; i < nnterms; i++) {
+        j = i + 1;
+        fderives[i] = buffer[i * nrules..j * nrules];
+      }
+    }
+
+    bool[][] firsts;
+    set_firsts(firsts);
+
+    for (symbol_number_t i = ntokens; i < nsyms; ++i)
+      for (symbol_number_t j = ntokens; j < nsyms; ++j)
+        if (firsts[i - ntokens][j - ntokens])
+          for (rule_number_t k = 0; derives[j - ntokens][k]; ++k)
+            fderives[i - ntokens][derives[j - ntokens][k].front.number] = true;
+  }
+
+  void print_fderives() {
+    import std.range.primitives;
+    import std.stdio;
+
+    write("FDERIVES\n");
+
+    for (symbol_number_t i = ntokens; i < nsyms; ++i) {
+      writef("  %s derives\n", symbols[i].tag);
+      foreach (r, flag; fderives[i - ntokens])
+        if (flag) {
+          writef("    %3d ", r);
+          if (rules[r].rhs.front >= 0)
+            for (item_number_t[] rhsp = rules[r].rhs; rhsp.front >= 0; rhsp.popFront)
+              writef(" %s", symbols[rhsp.front].tag);
+          else
+            writef(" %s", cast(dchar) 0x03b5);
+          write("\n");
+        }
+    }
+
+    write("\n\n");
+  }
+
+  void set_firsts(out bool[][] firsts) {
+    import std.range.primitives;
+
+    bool[] buffer = new bool[nnterms * nnterms];
+    firsts = new bool[][nnterms];
+    {
+      size_t j;
+      for (size_t i = 0; i < nnterms; i++) {
+        j = i + 1;
+        firsts[i] = buffer[i * nnterms..j * nnterms];
+      }
+    }
+    
+    for (symbol_number_t i = ntokens; i < nsyms; ++i)
+      for (symbol_number_t j = 0; derives[i - ntokens][j]; ++j) {
+        item_number_t sym = derives[i - ntokens][j].front.rhs.front;
+        if(sym >= ntokens)
+          firsts[i - ntokens][sym - ntokens] = true;
+      }
+    
+    for (size_t i = 0; i < firsts.length; i++)
+      for (size_t j = 0; j < firsts.length; j++)
+        if (firsts[j][i])
+          for (size_t k = 0; k < firsts[j].length; k++)
+            firsts[j][k] = firsts[j][k] || firsts[i][k];
+
+    for (size_t i = 0; i < firsts.length; i++)
+      firsts[i][i] = true;
+  }
+
+  void print_firsts(in bool[][] firsts) {
+    import std.stdio;
+
+    write("FIRSTS\n");
+
+    for (symbol_number_t i = ntokens; i < nsyms; ++i) {
+      writef("  %s firsts\n", symbols[i].tag);
+      foreach (j, flag; firsts[i - ntokens])
+        if (flag) writef("    %s\n", symbols[j + ntokens].tag);
+    }
+
+    write("\n\n");
+  }
+
+  void allocate_storage() {
+    allocate_itemsets();
+  }
+
+  void allocate_itemsets() {
+    size_t count = 0;
+    size_t[] symbol_count = new size_t[nsyms];
+
+    for (rule_number_t r = 0; r < nrules; ++r)
+      for (size_t i = 0; rules[r].rhs[i] >= 0; i++) {
+        symbol_number_t sym = rules[r].rhs[i];
+        count += 1;
+        symbol_count[sym] += 1;
+      }
+
+    kernel_base = new item_index_t[][nsyms];
+    kernel_items = new item_index_t[count];
+
+    count = 0;
+    for (symbol_number_t i = 0; i < nsyms; i++) {
+      kernel_base[i] = kernel_items[count..$];
+      count += symbol_count[i];
+    }
   }
 }
 
-struct gram_init {
+class gram_init {
+  parser_init root;
+
   symbol_t acceptsymbol;
   symbol_t errtoken;
   symbol_t undeftoken;
@@ -298,6 +305,9 @@ struct gram_init {
   symbol_list_t current_rule;
   symbol_list_t previous_rule_end;
 
+  int dummy_count = 0;
+  int order_of_appearance = 0;
+
   class symbol_list_t {
     symbol_t sym;
     symbol_list_t next;
@@ -307,7 +317,9 @@ struct gram_init {
     }
   }
 
-  void exec() {
+  this(parser_init r) {
+    root = r;
+
     gram_init_pre();
     token_init();
     nterm_init();
@@ -315,20 +327,19 @@ struct gram_init {
   }
 
   symbol_t declare_sym(symbol_t sym, symbol_class_t class_) {
-    static int order_of_appearance = 0;
     sym.content.class_ = class_;
     sym.order_of_appearance = ++order_of_appearance;
     return sym;
   }
 
   symbol_t symbol_get(string key) {
-    return symbol_table.require(key, new symbol_t(key));
+    if (key !in root.symbol_table)
+      root.symbol_table[key] = new symbol_t(key);
+    return root.symbol_table[key];
   }
 
   symbol_t dummy_symbol_get() {
     import std.format;
-
-    static int dummy_count = 0;
     return declare_sym(
       symbol_get(format("$@%d", ++dummy_count)),
       symbol_class_t.nterm_sym
@@ -346,7 +357,7 @@ struct gram_init {
     grammar_end = p;
 
     if (sym)
-      ++nritems;
+      ++root.nritems;
 
     return p;
   }
@@ -356,7 +367,7 @@ struct gram_init {
   }
 
   void grammar_current_rule_begin(symbol_t lhs) {
-    ++nrules;
+    ++root.nrules;
     previous_rule_end = grammar_end;
 
     current_rule = grammar_symbol_append(lhs);
@@ -379,8 +390,8 @@ struct gram_init {
     symbol_t dummy = dummy_symbol_get();
     symbol_list_t midrule = new symbol_list_t(dummy);
 
-    ++nrules;
-    ++nritems;
+    ++root.nrules;
+    ++root.nritems;
     
     if (previous_rule_end)
       previous_rule_end.next = midrule;
@@ -399,25 +410,25 @@ struct gram_init {
     import std.range.primitives;
 
     int itemno = 0;
-    ritem = new item_number_t[nritems];
+    root.ritem = new item_number_t[root.nritems];
 
     rule_number_t ruleno = 0;
-    rules = new rule_t[nrules];
+    root.rules = new rule_t[root.nrules];
 
     for (symbol_list_t p = grammar; p; p = p.next) {
       symbol_list_t lhs = p;
 
-      rules[ruleno].number = ruleno;
-      rules[ruleno].lhs = lhs.sym.content;
-      rules[ruleno].rhs = ritem[itemno..$];
+      root.rules[ruleno].number = ruleno;
+      root.rules[ruleno].lhs = lhs.sym.content;
+      root.rules[ruleno].rhs = root.ritem[itemno..$];
 
       size_t rule_length = 0;
       for (p = lhs.next; p.sym; p = p.next) {
         ++rule_length;
-        ritem[itemno++] = p.sym.content.number;
+        root.ritem[itemno++] = p.sym.content.number;
       }
 
-      ritem[itemno++] = -1 - ruleno;
+      root.ritem[itemno++] = -1 - ruleno;
       ++ruleno;
     }
   }
@@ -426,12 +437,12 @@ struct gram_init {
     acceptsymbol = symbol_get("$accept");
     acceptsymbol.order_of_appearance = 0;
     acceptsymbol.content.class_ = symbol_class_t.nterm_sym;
-    acceptsymbol.content.number = nnterms++;
+    acceptsymbol.content.number = root.nnterms++;
 
     errtoken = symbol_get("YYerror");
     errtoken.order_of_appearance = 0;
     errtoken.content.class_ = symbol_class_t.token_sym;
-    errtoken.content.number = ntokens++;
+    errtoken.content.number = root.ntokens++;
     {
       symbol_t alias_ = symbol_get("error");
       alias_.order_of_appearance = 0;
@@ -442,7 +453,7 @@ struct gram_init {
     undeftoken = symbol_get("YYUNDEF");
     undeftoken.order_of_appearance = 0;
     undeftoken.content.class_ = symbol_class_t.token_sym;
-    undeftoken.content.number = ntokens++;
+    undeftoken.content.number = root.ntokens++;
     {
       symbol_t alias_ = symbol_get("$undefined");
       alias_.order_of_appearance = 0;
@@ -461,8 +472,8 @@ struct gram_init {
     p.next = new symbol_list_t(null);
     p = p.next;
     p.next = grammar;
-    nrules += 1;
-    nritems += 3;
+    root.nrules += 1;
+    root.nritems += 3;
     grammar = initial_rule;
   }
 
@@ -484,24 +495,25 @@ struct gram_init {
     symbol_t start = start_symbols.sym;
     create_start_rule(null, start);
 
-    symbols_sorted = symbol_table.values
+    root.symbols_sorted = root.symbol_table.values
       .sort!("a.order_of_appearance < b.order_of_appearance")
       .array;
-    foreach (sym; symbols_sorted) {
+    foreach (sym; root.symbols_sorted) {
       sym_content_t s = sym.content;
 
       if (s.number == NUMBER_UNDEFINED)
-        s.number = s.class_ == symbol_class_t.token_sym ? ntokens++ : nnterms++;
+        s.number = s.class_ == symbol_class_t.token_sym
+          ? root.ntokens++ : root.nnterms++;
     }
 
-    nsyms = ntokens + nnterms;
-    symbols = new symbol_t[nsyms];
+    root.nsyms = root.ntokens + root.nnterms;
+    root.symbols = new symbol_t[root.nsyms];
 
-    foreach (sym; symbols_sorted) {
+    foreach (sym; root.symbols_sorted) {
       if (sym.content.class_ == symbol_class_t.nterm_sym)
-        sym.content.number += ntokens;
+        sym.content.number += root.ntokens;
 
-      symbols[sym.content.number] = sym.content.symbol;
+      root.symbols[sym.content.number] = sym.content.symbol;
     }
 
     packgram();
