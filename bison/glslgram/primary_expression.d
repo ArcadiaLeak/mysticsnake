7406d78cdd405599de89e63ba6f584b024353db6
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-02-09 16:10:09 +0300

    primary_expression decltoken

--- bison/glslgram/primary_expression.d
+++ bison/glslgram/primary_expression.d
@@ -2,6 +2,14 @@ module bison.glslgram.primary_expression;
 
 import bison;
 
+private void declrule(string tags ...) {
+  foreach (tag; tags) {
+    grammar_current_rule_begin(symbol_get("primary_expression"));
+    grammar_current_rule_symbol_append(symbol_get(tag));
+    grammar_current_rule_end();
+  }
+}
+
 void primary_expression() {
   declare_sym(symbol_get("primary_expression"), symbol_class_t.nterm_sym);
 
@@ -15,55 +23,8 @@ void primary_expression() {
   grammar_current_rule_symbol_append(symbol_get("RIGHT_PAREN"));
   grammar_current_rule_end();
 
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("FLOATCONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("INTCONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("UINTCONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("BOOLCONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("STRING_LITERAL"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("INT32CONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("UINT32CONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("INT64CONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("UINT64CONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("INT16CONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("UINT16CONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("DOUBLECONSTANT"));
-  grammar_current_rule_end();
-
-  grammar_current_rule_begin(symbol_get("primary_expression"));
-  grammar_current_rule_symbol_append(symbol_get("FLOAT16CONSTANT"));
-  grammar_current_rule_end();
+  declrule("FLOATCONSTANT", "INTCONSTANT", "UINTCONSTANT", "BOOLCONSTANT");
+  declrule("STRING_LITERAL", "INT32CONSTANT", "UINT32CONSTANT", "INT64CONSTANT");
+  declrule("UINT64CONSTANT", "INT16CONSTANT", "UINT16CONSTANT", "DOUBLECONSTANT");
+  declrule("FLOAT16CONSTANT");
 }
