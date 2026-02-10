95c81f22308cecba6087d9c4f20878e65978e4e1
Evgenii Kosmogradskii-Ivanov <kosmogradsky@yandex.ru>
2026-02-11 00:44:32 +0300

    spirv_requirements_parameter

--- bison/glslgram/spirv_requirements_parameter.d
+++ bison/glslgram/spirv_requirements_parameter.d
@@ -1,4 +1,22 @@
 module bison.glslgram.spirv_requirements_parameter;
 import bison;
 
+auto spirv_requirements_parameter() {
+  declare_sym(symbol_get("spirv_requirements_parameter"), symbol_class_t.nterm_sym);
 
+  grammar_current_rule_begin(symbol_get("spirv_requirements_parameter"));
+  grammar_current_rule_symbol_append(symbol_get("IDENTIFIER"));
+  grammar_current_rule_symbol_append(symbol_get("EQUAL"));
+  grammar_current_rule_symbol_append(symbol_get("LEFT_BRACKET"));
+  grammar_current_rule_symbol_append(symbol_get("spirv_extension_list"));
+  grammar_current_rule_symbol_append(symbol_get("RIGHT_BRACKET"));
+  grammar_current_rule_end();
+
+  grammar_current_rule_begin(symbol_get("spirv_requirements_parameter"));
+  grammar_current_rule_symbol_append(symbol_get("IDENTIFIER"));
+  grammar_current_rule_symbol_append(symbol_get("EQUAL"));
+  grammar_current_rule_symbol_append(symbol_get("LEFT_BRACKET"));
+  grammar_current_rule_symbol_append(symbol_get("spirv_capability_list"));
+  grammar_current_rule_symbol_append(symbol_get("RIGHT_BRACKET"));
+  grammar_current_rule_end();
+}
